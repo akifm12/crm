01ab16425decb3ebd7cf38646e34367fed6d43c2
akifm12 <akif@yahoo.com>
2026-05-01 14:44:05 +0400

    quotation template

--- app/Models/QuotationTemplate.php
+++ app/Models/QuotationTemplate.php
@@ -3,6 +3,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class QuotationTemplate extends Model {
-    protected $fillable = ['name','service_type','description','line_items','terms','validity_days','is_active','created_by'];
-    protected $casts = ['line_items'=>'array','is_active'=>'boolean'];
+    protected $fillable = [
+        'name','service_type','description',
+        'line_items','terms','validity_days',
+        'is_active','created_by'
+    ];
+    protected $casts = [
+        'line_items' => 'array',
+        'is_active'  => 'boolean',
+    ];
 }
