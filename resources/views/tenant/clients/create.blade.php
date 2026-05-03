3f5b17cb15531dab255eaaf530ace3320410ce02
akifm12 <akif@yahoo.com>
2026-05-03 20:55:12 +0400

    display issues

--- resources/views/tenant/clients/create.blade.php
+++ resources/views/tenant/clients/create.blade.php
@@ -524,7 +524,7 @@
 <script>
 function clientForm() {
     return {
-        clientType: 'corporate_local',
+        clientType: '{{ array_key_first($sector['client_types']) }}',
         step: 1,
         indStep: 1,
         stepErrors:    {1:false,2:false,3:false,4:false,5:false,6:false,7:false},
